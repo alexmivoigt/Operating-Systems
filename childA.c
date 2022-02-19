4ae97c94c447f9702a9312e07a91f027d410a85d
alexmivoigt <46804890+alexmivoigt@users.noreply.github.com>
2022-02-19 15:42:32 -0600

    Update childA.c

--- childA.c
+++ childA.c
@@ -1,4 +1,10 @@
-main() {
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
   printf("Child A initialized\n");  
   for (int i = 0; i <= 10000; i++) {
     if (i % 1000 == 0) {
