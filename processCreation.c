6de7f2777d1845ab10fc27ccc283f27351db1617
alexmivoigt <46804890+alexmivoigt@users.noreply.github.com>
2022-02-20 15:19:05 -0600

    Update processCreation.c

--- processCreation.c
+++ processCreation.c
@@ -11,39 +11,23 @@ int main()
     
     printf("Alex Voigt\n");
     
-    child_a = fork();
+    child_a = fork(); //fork Parent to Child A
     
-    if (child_a == 0) {
-        printf("Child A initialized\n");
-        
-        for (int i = 0; i <= 10000; i++) {
-            if (i % 1000 == 0) {
-                printf("Child A reached iteration %d\n", i);
-            }
-        }
-        
-        exit(0);
+    if (child_a == 0) { //execute Child A code
+        execl(childA, childA, null);
     } else {
-        child_b = fork();
-        
-        if (child_b == 0) {
-            printf("Child B initialized\n");
-            
-            for (int i = 0; i <= 10000; i++) {
-                if (i % 1000 == 0) {
-                    printf("Child B reached iteration %d\n", i);
-                }
-            }
-            
-            exit(0);
-        } else {
-            int status;
-            pid_t pid;
-            
-            for (int i = 0; i < 2; i++) {
-                pid = wait(&status);
-            }
-            return(0);
-        }
+        child_b = fork(); //fork Parent to Child B
     }
-}
+    
+    if(child_b == 0) { //execute Child B code
+        execl(childB, childB, null);
+    } else { //wait for children to finish
+        int status;
+        pid_t pid;
+            
+        for (int i = 0; i < 2; i++) {
+            pid = wait(&status);
+            printf("Child terminated\n");
+        }
+        return(0);
+    }
